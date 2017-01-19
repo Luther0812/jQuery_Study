348daf37b1a977cfaff10e74a94c750c56d7a14d
Luther0812 <luther0812@163.com>
2017-01-19 11:15:52 +0800

    jQueryUI

---
+++ jQueryUI/005/action.php
@@ -0,0 +1,8 @@
+<?php
+    $name = $_REQUEST["name"];
+    $surname = $_REQUEST["surname"];
+    $name = utf8_decode($name);
+    $surname = utf8_decode($surname);
+
+    $txt = "<p>Tab Content Sent By Server</p>";
+?>
---
+++ jQueryUI/006/action.php
@@ -0,0 +1,5 @@
+<?php
+	$txt = "<p>Tab Content Sent By Server</p>";
+	$txt = utf8_encode($txt);
+    echo($txt);
+?>
